5fc967e91f5e610175e6c070e25e08feb52e0221
DmitrySar <youshin.d@gmail.com>
2021-02-05 10:57:57 +0300

    Initial commit

---
+++ src/main/java/diplom/arduinoHttpSwitcher/ArduinoHttpSwitcherApplication.java
@@ -0,0 +1,13 @@
+package diplom.arduinoHttpSwitcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArduinoHttpSwitcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArduinoHttpSwitcherApplication.class, args);
+	}
+
+}
