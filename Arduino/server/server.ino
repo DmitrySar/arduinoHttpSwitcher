d5261c54c622ec9d1b8690bac39143a961bacf3b
DmitrySar <youshin.d@gmail.com>
2021-02-05 12:08:25 +0300

    add arduino sketch

---
+++ Arduino/server/server.ino
@@ -0,0 +1,59 @@
+// parameters
+// 10 - switch 1 is off
+// 11 - switch 1 is on
+// 20 - switch 2 is off
+// 21 - switch 2 is on
+// 13 - read status from switch1
+// 23 - read status from switch2
+
+#include "etherShield.h"
+#include "ETHER_28J60.h"
+ 
+static uint8_t mac[6] = {0x54, 0x55, 0x58, 0x10, 0x00, 0x24};   
+static uint8_t ip[4] = {192, 168, 1, 63};                      
+static uint16_t port = 80;
+const int SW1 = 3;
+const int SW2 = A2;
+ 
+ETHER_28J60 ethernet;
+ 
+void setup() {
+  pinMode(SW1, OUTPUT);
+  pinMode(SW2, OUTPUT);
+  digitalWrite(SW1, LOW);
+  digitalWrite(SW2, LOW);
+  ethernet.setup(mac, ip, port);  
+}
+
+
+void loop() {
+  char* param;
+  if (param = ethernet.serviceRequest()) {
+      if (strcmp(param, "10") == 0) {
+        ethernet.print(switcher(SW1, false));
+    } else if (strcmp(param, "11") == 0) {
+        ethernet.print(switcher(SW1, true));
+    } else if (strcmp(param, "20") == 0) {
+        ethernet.print(switcher(SW2, false));
+    } else if (strcmp(param, "21") == 0) {
+        ethernet.print(switcher(SW2, true));
+    } else if (strcmp(param, "13") == 0) {
+        ethernet.print(digitalRead(SW1));
+    } else if (strcmp(param, "23") == 0) {
+        ethernet.print(digitalRead(SW2));
+    }
+
+    ethernet.respond();
+  }
+  delay(50);
+}
+
+
+boolean switcher(int sw, boolean key) {
+  if (key) {
+    digitalWrite(sw, HIGH);
+  } else {
+    digitalWrite(sw, LOW);
+  }
+  return digitalRead(sw);
+}
