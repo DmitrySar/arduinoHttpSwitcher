7022871eb668329667d1ee330e1e572a5bc9424e
DmitrySar <youshin.d@gmail.com>
2021-02-07 13:35:13 +0300

    review

--- src/main/java/diplom/arduinoHttpSwitcher/controller/ProxyController.java
+++ src/main/java/diplom/arduinoHttpSwitcher/controller/ProxyController.java
@@ -1,16 +1,10 @@
 package diplom.arduinoHttpSwitcher.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 @RestController
 public class ProxyController {
